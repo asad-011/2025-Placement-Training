19ced715a850fa883bd9e3e1066b22d85244a446
asad-011 <asadmohd543@gmail.com>
2025-06-25 14:03:10 +0530

    Create Linked List Cycle.cpp

---
+++ DSA/25 JNUE/Linked List Cycle.cpp
@@ -0,0 +1,31 @@
+/**
+ * Definition for singly-linked list.
+ * struct ListNode {
+ *     int val;
+ *     ListNode *next;
+ *     ListNode(int x) : val(x), next(NULL) {}
+ * };
+ */
+class Solution {
+public:
+    bool hasCycle(ListNode *head) {
+        // set<ListNode*> st;                //Brute
+        // ListNode* temp=head;
+        // while(temp){
+        //     if(st.find(temp)!=st.end()) return true;
+        //     st.emplace(temp);
+        //     temp=temp->next;
+        // }
+        // return false;
+
+        //optimal  tortoise and Hare Algorithm 
+        ListNode* slow=head;
+        ListNode* fast=head;
+        while(fast!=NULL && fast->next!=NULL){
+            fast=fast->next->next;
+            slow=slow->next;
+            if(slow==fast) return true;
+        }
+        return false;
+    }
+};
