482e3c62f68ce2f2cc21698db3136ae533573518
asad-011 <asadmohd543@gmail.com>
2025-07-01 20:48:04 +0530

    Create Next Greater Element II.cpp

---
+++ DSA/1 JULY/Next Greater Element II.cpp
@@ -0,0 +1,15 @@
+class Solution {
+public:
+    vector<int> nextGreaterElements(vector<int>& nums) {
+        stack<int> st;
+        int n=nums.size();
+        vector<int> nge(nums.size(),-1);
+        for(int i=2*nums.size()-1 ; i>=0 ; i--){
+            while(!st.empty() && st.top()<=nums[i%n]) {st.pop();}
+            if(st.empty()) nge[i%n]=-1;
+            else nge[i%n]=st.top();
+            st.push(nums[i%n]);
+        }
+        return nge;
+    }
+};
