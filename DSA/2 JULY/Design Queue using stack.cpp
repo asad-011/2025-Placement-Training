cab7480a57dd0b3c5a77af5626fc0be1d667217d
asad-011 <asadmohd543@gmail.com>
2025-07-02 19:53:48 +0530

    Update Design Queue using stack.cpp

--- DSA/2 JULY/Design Queue using stack.cpp
+++ DSA/2 JULY/Design Queue using stack.cpp
@@ -1,1 +1,52 @@
 
+class MyQueue {
+    stack<int> st;
+    stack<int> temp;
+public:
+    MyQueue() {  
+    }
+    
+    void push(int x) {
+        st.push(x);
+    }
+    
+    int pop() {
+        while(!st.empty()){
+            temp.push(st.top());
+            st.pop();
+        }
+        int pop=temp.top();
+        temp.pop();
+        while(!temp.empty()){
+            st.push(temp.top());
+            temp.pop();
+        }
+        return pop; 
+    }
+    
+    int peek() {
+        while(!st.empty()){
+            temp.push(st.top());
+            st.pop();
+        }
+        int peek=temp.top();
+        while(!temp.empty()){
+            st.push(temp.top());
+            temp.pop();
+        }
+        return peek;
+    }
+    
+    bool empty() {
+        return st.empty()==0;
+    }
+};
+
+/**
+ * Your MyQueue object will be instantiated and called as such:
+ * MyQueue* obj = new MyQueue();
+ * obj->push(x);
+ * int param_2 = obj->pop();
+ * int param_3 = obj->peek();
+ * bool param_4 = obj->empty();
+ */
